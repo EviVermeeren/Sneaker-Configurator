e58a7a590a5c022d0f5d69e6fe5b77e4d658fbc7
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-19 19:56:32 +0100

    feat: import new component ShoePage

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "./components/Header.vue";
+import ShoePage from "./components/ShoePage.vue";
 </script>
 
 <template>
   <Header />
+  <ShoePage />
 </template>
 
 <style scoped></style>

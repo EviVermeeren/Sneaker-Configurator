afd5e369cb6a0895890e59aed686a6e68db546b9
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-19 19:33:18 +0100

    feat: add header

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <div class="shoe-desc">
+      <img src="../assets/SWEAR_Logo.webp" alt="SWEAR-LOGO" />
+    </div>
+    <a class="link" href="">Admin</a>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #fff;
+  color: #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  margin: 0;
+  border-bottom: #000;
+  border-width: 0 0 2px 0;
+  border-style: solid;
+}
+
+.shoe-desc {
+  flex-direction: column;
+  align-items: start;
+  padding: 43px;
+  background-color: #d6ff38;
+}
+
+.link {
+  font-family: "Inter-Light", Helvetica;
+  font-weight: 300;
+  color: #000000;
+  font-size: 24px;
+  letter-spacing: 0.96px;
+  line-height: normal;
+  text-decoration: underline;
+  margin-right: 43px;
+}
+</style>

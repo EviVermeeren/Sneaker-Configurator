7be8a8ca432824d6d508601e010556d34d25d4f0
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-25 14:59:23 +0100

    feat: show 3D model in shoedetail to see placed order

--- src/components/ShoeDetails.vue
+++ src/components/ShoeDetails.vue
@@ -1,15 +1,19 @@
 <template>
+  <div class="canvas-container" ref="canvasContainer"></div>
+
   <div class="bgc">
     <div class="flex">
       <div>
         <img src="../assets/shoe.webp" alt="SHOE" />
       </div>
+
       <div>
         <h1 id="type">{{ shoe.shoeType }}</h1>
         <h2>{{ shoe.userName }}</h2>
         <p>Shipping adress: {{ shoe.userAddress }}</p>
         <p>Size: {{ shoe.shoeSize }}</p>
         <p>Status: {{ shoe.status }}</p>
+        <p>Price: {{ shoe.shoeColorSole }}</p>
         <div class="buttons">
           <button>Update status to “in production”</button>
           <button>Update status to “in transit”</button>
@@ -25,15 +29,116 @@
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { TextureLoader } from "three/src/loaders/TextureLoader.js";
+
 export default {
   data() {
     return {
       shoeId: this.$route.params.id,
-      shoe: {},
+      shoe: {
+        jewel: null,
+        shoeColorLaces: null,
+        shoeColorPanelDown: null,
+        shoeColorPanelUp: null,
+        shoeColorSole: null,
+        shoeMaterialPanelDown: null,
+        shoeMaterialPanelUp: null,
+        shoeSize: null,
+        shoeType: null,
+        status: null,
+        userAddress: null,
+        userEmail: null,
+        userName: null,
+      },
     };
   },
-  mounted() {
-    this.fetchShoes();
+  async mounted() {
+    await this.fetchShoes(); // Wait for the fetch to complete
+    const windowWidth = window.innerWidth * 2;
+    const ratio = windowWidth / window.innerHeight;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, ratio, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight * 0.5);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    this.$refs.canvasContainer.appendChild(renderer.domElement);
+
+    camera.position.z = 7;
+
+    const loadingManager = new THREE.LoadingManager();
+
+    const gltfLoader = new GLTFLoader(loadingManager);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.enablePan = false;
+
+    scene.background = new THREE.Color(0xffffff);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.7);
+    const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1.7);
+    const directionalLight3 = new THREE.DirectionalLight(0xffffff, 1.7);
+    const directionalLight4 = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(0, 0, 1);
+    directionalLight2.position.set(0, 0, -1);
+    directionalLight3.position.set(0, 1, 0);
+    directionalLight4.position.set(-1, 0, 0);
+    scene.add(directionalLight);
+    scene.add(directionalLight2);
+    scene.add(directionalLight3);
+    scene.add(directionalLight4);
+
+    let shoe;
+
+    const updateShoeColors = () => {
+      const shoeColorLaces = this.shoe.shoeColorLaces;
+
+      if (shoeColorLaces) {
+        const shoeLaces = shoe.getObjectByName("laces");
+
+        if (shoeLaces) {
+          shoeLaces.material.color.set(shoeColorLaces);
+          console.log("Shoe color set:", shoeColorLaces);
+        } else {
+          console.error("Shoe laces not found in the 3D model.");
+        }
+      } else {
+        console.warn(
+          "Shoe color information is missing or null. Check the API response:",
+          this.shoe
+        );
+      }
+    };
+
+    gltfLoader.load("/models/new-shoe.glb", (gltf) => {
+      shoe = gltf.scene;
+      shoe.scale.set(2.5, 2.5, 2.5);
+
+      shoe.rotation.order = "YXZ";
+
+      shoe.rotation.x = 0.5;
+      shoe.rotation.y = 1.5;
+
+      shoe.position.z = 0;
+      shoe.position.y = -0.5;
+      shoe.position.x = -0.5;
+
+      scene.add(shoe);
+
+      // Move the updateShoeColors call here, after the shoe model is loaded
+      updateShoeColors();
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+
+    animate();
   },
   methods: {
     async fetchShoes() {
@@ -45,6 +150,7 @@ export default {
 
         if (data.status === "success") {
           this.shoe = data.data.shoe || {}; // Use empty object if undefined
+          console.log(this.shoe);
         } else {
           console.error("Error fetching shoes:", data.message);
         }

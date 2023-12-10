4f4810d3a46071d5beaa7b85ff74f558bef58719
ArneSamson <arnesamson007@gmail.com>
2023-12-10 14:27:19 +0100

    feat: add logic to play sound when clicking the button

--- src/components/HeaderMinimal.vue
+++ src/components/HeaderMinimal.vue
@@ -7,12 +7,40 @@
         <img class="shoe-desc__img" src="/media/sound.svg" alt="" />
       </button>
     </header>
-</template>
+  </template>
   
 <script>
+import { Howl } from "howler";
 
+export default {
+data() {
+    return {
+    sound: null,
+    isPlaying: false,
+    };
+},
+methods: {
+    initSound() {
+    this.sound = new Howl({
+        src: ["/media/sonicbranding.mp3"], // Replace with the path to your sound file
+        loop: true, // Set to true if you want the sound to loop
+    });
+    },
+    toggleSound() {
+    if (!this.sound) {
+        this.initSound();
+    }
+
+    if (this.isPlaying) {
+        this.sound.pause();
+    } else {
+        this.sound.play();
+    }
+
+    this.isPlaying = !this.isPlaying;
+    },
+},
+};
 </script>
-
-<style>
-
-</style>
+  
+  

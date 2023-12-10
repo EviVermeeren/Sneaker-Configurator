667dde3a87eca5231739c2323b90fe3ae3d85f37
ArneSamson <arnesamson007@gmail.com>
2023-12-10 14:24:50 +0100

    feat: add a second type of header

---
+++ src/components/HeaderMinimal.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+      <div class="shoe-desc">
+        <img src="/media/logo.webp" alt="SWEAR-LOGO" />
+      </div>
+    </header>
+</template>
+  
+<script>
+
+</script>
+
+<style>
+
+</style>

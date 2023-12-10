3073b7aaea62ac406ebc6092e380c2374e3fae70
ArneSamson <arnesamson007@gmail.com>
2023-12-10 14:25:21 +0100

    feat: add button to toggle background sound

--- src/components/HeaderMinimal.vue
+++ src/components/HeaderMinimal.vue
@@ -3,6 +3,9 @@
       <div class="shoe-desc">
         <img src="/media/logo.webp" alt="SWEAR-LOGO" />
       </div>
+      <button @click="toggleSound">
+        <img class="shoe-desc__img" src="/media/sound.svg" alt="" />
+      </button>
     </header>
 </template>
   
